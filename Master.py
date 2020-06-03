682c5c5a0cc64a9878eaeb2464d0be436fb394fa
Irwanto Wibowo <rinapratama335@gmail.com>
2020-06-04 02:40:33 +0700

    konversi tipe data dengan python

--- Master.py
+++ Master.py
@@ -1,21 +1,50 @@
-from ctypes import c_double
+# Integer
+print("=========== Konversi Dari Tipe Data Integer ===========")
+data_int = 9
+print("data =", data_int, ", type", type(data_int))
 
-data_integer = 1
-print("data :", data_integer, "bertipe", type(data_integer))
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print("data =", data_float, ", type", type(data_float))
+print("data =", data_str, ", type", type(data_str))
+print("data =", data_bool, ", type", type(data_bool))
 
-data_float = 1.5
-print("data :", data_float, "bertipe", type(data_float))
+print("\n")
 
-data_string = "irwanto"
-print("data :", data_string, "bertipe", type(data_string))
+print("=========== Konversi Dari Tipe Data Float ===========")
+data_float = 9.5
+print("data =", data_float, ", type", type(data_float))
 
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+print("data =", data_int, ", type", type(data_int))
+print("data =", data_str, ", type", type(data_str))
+print("data =", data_bool, ", type", type(data_bool))
+
+print("\n")
+
+print("=========== Konversi Dari Tipe Data Boolean ===========")
 data_bool = True
-print("data :", data_bool, "bertipe", type(data_bool))
+print("data =", data_bool, ", type", type(data_bool))
 
+data_int = int(data_bool)
+data_str = str(data_bool)
+data_float = float(data_bool)
+print("data =", data_int, ", type", type(data_int))
+print("data =", data_str, ", type", type(data_str))
+print("data =", data_float, ", type", type(data_float))
 
-data_complex = complex(5, 6)
-print("data :", data_complex, "bertipe", type(data_complex))
+print("\n")
 
-# kita bisa menggunakan tipe data dari bahasa C
-data_c_double = c_double(10.5)
-print("data :", data_c_double, "bertipe", type(data_c_double))
+print("=========== Konversi Dari Tipe Data String ===========")
+data_str = "0"
+print("data =", data_str, ", type", type(data_str))
+
+data_int = int(data_str)
+data_bool = bool(data_str)
+data_float = float(data_str)
+print("data =", data_int, ", type", type(data_int))
+print("data =", data_bool, ", type", type(data_bool))
+print("data =", data_float, ", type", type(data_float))
