1f181f24e13b555caf694d270893b7cc497d73d4
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-06-04 01:50:11 +0700

    mengenal tipe data di python (#3)

--- Master.py
+++ Master.py
@@ -1,12 +1,21 @@
-a = 10
-x = 5
-panjang = 1000
+from ctypes import c_double
 
-print("Nilai a =", a)
-print("Nilai x =", x)
-print("Nilai panjang =", panjang)
+data_integer = 1
+print("data :", data_integer, "bertipe", type(data_integer))
 
-a = 8
-print("Nilai a =", a)
-b = a
-print("Nilai b =", b)
+data_float = 1.5
+print("data :", data_float, "bertipe", type(data_float))
+
+data_string = "irwanto"
+print("data :", data_string, "bertipe", type(data_string))
+
+data_bool = True
+print("data :", data_bool, "bertipe", type(data_bool))
+
+
+data_complex = complex(5, 6)
+print("data :", data_complex, "bertipe", type(data_complex))
+
+# kita bisa menggunakan tipe data dari bahasa C
+data_c_double = c_double(10.5)
+print("data :", data_c_double, "bertipe", type(data_c_double))
