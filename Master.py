e83128463de3197ede95a9fc0aafd5a052813033
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-06-04 01:02:30 +0700

    memahami cara kerja python dan membuat bytecode python (#1)

--- Master.py
+++ Master.py
@@ -1,1 +1,21 @@
+import time
+start_time = time.time()
+
+print("Hello")
+print('World')
 print("Hello World")
+
+print('Hallo ganteng')
+# ini adalah komentar
+a = 10  # ini juga komentar
+"""
+saya sefang membuat
+multiple komentar
+dahsyattt
+"""
+print(a)
+
+for i in range(1, 1000):
+    a = 10
+
+print("Waktu eksekusi ", time.time() - start_time, " detik")
