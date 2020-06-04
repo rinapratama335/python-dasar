50bb198786efc162fd53492a30cc2a946ce24ace
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-06-04 16:41:03 +0700

    latihan sederhana membuat konversi suhu (#6)

--- Master.py
+++ Master.py
@@ -1,8 +1,36 @@
-data = input("Maskkan data inputan : ")
-print("Data yang kamu masukkan :", data, "type :", type(data))
+print("====================== Membuat program konversi suhu ======================")
+print("\n")
 
-angka = int(input("Masukkan anka : "))
-print("Angka yang kamu masukkan :", angka, "type :", type(angka))
+# print("========== Dari Celsius ==========")
+# celcius = float(input("Masukkan suhu dalam celcius : "))
+# print("Suhu : ", celcius, "derajat celcius")
 
-data_bool = bool(int(input("Masukkan data boolean : ")))
-print("Biner yang kamu masukkan :", data_bool, "type :", type(data_bool))
+# # konversi ke reamur
+# reamur = (4 / 5) * celcius
+# print("Suhu dalam reamur : ", reamur, "derajat reamur")
+
+# # konversi ke fahrenheit
+# fahrenheit = ((9 / 5) * celcius) + 32
+# print("Suhu dalam fahrenheit : ", fahrenheit, "derajat fahrenheit")
+
+# # konversi ke kelvin
+# kelvin = celcius + 273
+# print("Suhu dalam kelvin : ", kelvin, "derajat kelvin")
+
+
+# print("========== Dari Fahrenheit ==========")
+# suhu_fahrenheit = float(input("Masukkan suhu dalam fahrenheit : "))
+
+# print("Suhu yang kamu input :", suhu_fahrenheit, 'derajat fahrenheit')
+
+# suhu_kelvin = (((5 / 9) * suhu_fahrenheit) - 32) + 273
+# print("Suhu dalam kelvin : ", suhu_kelvin, "derajat kelvin")
+
+
+print("========== Dari Kelvin ==========")
+suhu_kelvin = float(input("Masukkan suhu dalam kelvin : "))
+
+print("Suhu yang kamu input :", suhu_kelvin, 'derajat kelvin')
+
+suhu_fahrenheit = ((9 / 5) * (suhu_kelvin - 273)) + 32
+print("Suhu dalam fahrenheit : ", suhu_fahrenheit, "derajat fahrenheit")
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-## Mengambil Input Data Dari User
+## Membuat Program Sederhana Konversi Suhu
 
-Dengan menggunakan keyword `input()` kita bisa mengambil data inputan yang dimasukkan oleh user. Yang perlu kita ketahui adalah data yang dimasukkan akan bertipe string.
-Untuk merubah ke dalam tipe data yang lain maka kia bisa melakukan casting (konversi tipe data).
+Tabel Rumus:
+
+1. Celcius
+   ke Reamur = (4/5) * celcius
+   ke Fahrenheit = (9/5)*celcius+32
+   ke Kelvin = celsius + 273
+
+2. Reamur
+   ke Celcius = (5/4) _ reamur
+   ke Fahrenheit = (9/4) _ reamur + 32
+   ke Kelvin = (5/4) \* reamur + 273
+
+3. Fahrenheit
+   ke Celcius = (5/9) _ (fahrenheit-32)
+   ke Reamur = (4/9) _ (fahrenheit - 32)
+
+4. Kelvin
+   ke Celcius = kelvin - 273
+   ke Reamur = (4/5) \* (kelvin - 273)
