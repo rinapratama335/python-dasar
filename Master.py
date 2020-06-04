3cede215b52cbbd37275db652808772f6170a03c
Irwanto Wibowo <rinapratama335@gmail.com>
2020-06-04 14:57:11 +0700

    mengambil inputan data dari user

--- Master.py
+++ Master.py
@@ -1,50 +1,8 @@
-# Integer
-print("=========== Konversi Dari Tipe Data Integer ===========")
-data_int = 9
-print("data =", data_int, ", type", type(data_int))
+data = input("Maskkan data inputan : ")
+print("Data yang kamu masukkan :", data, "type :", type(data))
 
-data_float = float(data_int)
-data_str = str(data_int)
-data_bool = bool(data_int)
-print("data =", data_float, ", type", type(data_float))
-print("data =", data_str, ", type", type(data_str))
-print("data =", data_bool, ", type", type(data_bool))
+angka = int(input("Masukkan anka : "))
+print("Angka yang kamu masukkan :", angka, "type :", type(angka))
 
-print("\n")
-
-print("=========== Konversi Dari Tipe Data Float ===========")
-data_float = 9.5
-print("data =", data_float, ", type", type(data_float))
-
-data_int = int(data_float)
-data_str = str(data_float)
-data_bool = bool(data_float)
-print("data =", data_int, ", type", type(data_int))
-print("data =", data_str, ", type", type(data_str))
-print("data =", data_bool, ", type", type(data_bool))
-
-print("\n")
-
-print("=========== Konversi Dari Tipe Data Boolean ===========")
-data_bool = True
-print("data =", data_bool, ", type", type(data_bool))
-
-data_int = int(data_bool)
-data_str = str(data_bool)
-data_float = float(data_bool)
-print("data =", data_int, ", type", type(data_int))
-print("data =", data_str, ", type", type(data_str))
-print("data =", data_float, ", type", type(data_float))
-
-print("\n")
-
-print("=========== Konversi Dari Tipe Data String ===========")
-data_str = "0"
-print("data =", data_str, ", type", type(data_str))
-
-data_int = int(data_str)
-data_bool = bool(data_str)
-data_float = float(data_str)
-print("data =", data_int, ", type", type(data_int))
-print("data =", data_bool, ", type", type(data_bool))
-print("data =", data_float, ", type", type(data_float))
+data_bool = bool(int(input("Masukkan data boolean : ")))
+print("Biner yang kamu masukkan :", data_bool, "type :", type(data_bool))
